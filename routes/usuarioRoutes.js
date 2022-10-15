1c206c95f0844a6f8f19ec68c77984ac885b7a8a
Matteo <mspedicatov@gmail.com>
2022-10-16 01:27:39 +0200

    primer commit

---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+const router = express.Router();
+import { 
+    registrar,
+    autenticar,
+    confirmar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword,
+    perfil,
+} from "../controllers/usuarioController.js";
+
+import checkAuth from "../middleware/checkAuth.js";
+
+// Autenticación, Registro y Confirmación de Usuarios
+router.post("/", registrar); // Crea un nuevo Usuario
+router.post("/login", autenticar);
+router.get("/confirmar/:token", confirmar);
+router.post("/olvide-password", olvidePassword);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+
+router.get("/perfil", checkAuth, perfil);
+
+export default router;
